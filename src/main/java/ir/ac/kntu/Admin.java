e6a70ed59346ad953e03ba22835b9ec87e3fa033
Mmd Abed <1mmmlabd@gmai.com>
2023-04-25 17:48:24 +0430

    AdminLogin checked.

---
+++ src/main/java/ir/ac/kntu/Admin.java
@@ -0,0 +1,10 @@
+package ir.ac.kntu;
+
+public class Admin {
+    String username;
+    String password;
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/main/java/ir/ac/kntu/StoreProgram.java
@@ -0,0 +1,77 @@
+package ir.ac.kntu;
+
+import java.util.Scanner;
+
+public class StoreProgram {
+
+    public static void makeHashie(){
+        System.out.println("._._._._._.");
+    }
+
+    public static void displayMenu(){
+        makeHashie();
+        System.out.println("1.Admin");
+        System.out.println("2.User");
+        makeHashie();
+        Scanner sc = new Scanner(System.in);
+        int ans = sc.nextInt();
+        switch ( ans ){
+            case 1:
+                adminLogIn();
+        }
+    }
+
+    private static void tryInputAgain(){
+        System.out.println("Wrong Input!");
+        System.out.println("1.Retry");
+        System.out.println("2.Return");
+    }
+
+    private static void adminLogIn() {
+        Scanner sc = new Scanner(System.in);
+        Admin admin = new Admin("aaa","123");
+        System.out.println("Enter username:");
+        String enteredUser = sc.nextLine();
+        if(admin.username.equals(enteredUser)){
+            makeHashie();
+            System.out.println("Enter password");
+            String enteredPass = sc.nextLine();
+            makeHashie();
+            while(!admin.password.equals(enteredPass)){
+                tryInputAgain();
+                String ans = sc.nextLine();
+                switch (ans){
+                    case "1":{
+                        System.out.println("Enter password");
+                        enteredPass = sc.nextLine();
+                        makeHashie();
+                        break;
+                    }
+                    case "2":{
+                        displayMenu();
+                        break;
+                    }
+                }
+            }
+        }else {
+            tryInputAgain();
+            String ans = sc.nextLine();
+            switch (ans) {
+                case "1":{
+                    adminLogIn();
+                    break;
+                }
+                case "2":{
+                    displayMenu();
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        displayMenu();
+    }
+}
